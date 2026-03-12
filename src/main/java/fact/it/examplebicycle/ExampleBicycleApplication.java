eb7d1ee44f2a906877adc498922be6f761afd73a
Liamwibo <r1092482@student.thomasmore.be>
2026-03-12 14:24:42 +0100

    best push of the year

--- src/main/java/fact/it/examplebicycle/ExampleBicycleApplication.java
+++ src/main/java/fact/it/examplebicycle/ExampleBicycleApplication.java
@@ -16,7 +16,7 @@ public class ExampleBicycleApplication {
         myBicycle.setYear(2016);
         myBicycle.setRentalPrice(4.5);
 
-        System.out.println("You created a Bicycle-object with the following values:");
+        System.out.println("JESSE HOUD VAN MANNEN!");
         System.out.println("The type of your bicycle is: " + myBicycle.getType());
         System.out.println("The year of your bicycle is: " + myBicycle.getYear());
         System.out.println("The rental price of your bicycle is: " + myBicycle.getRentalPrice());
